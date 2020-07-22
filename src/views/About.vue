f5d36cf1b35226f81182402036b1ae8287056446
MIchael Fernandez <michaelfernandez@MIchaels-MBP.home>
2020-07-21 22:48:30 -0400

    about test

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,17 @@
   <div class="about">
     <h1>This is an about page</h1>
     <p>Github test</p>
+    <p>{{test}}</p>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data(){
+    return {
+      msg: 'test'
+    }
+  }
+})
+</script>
