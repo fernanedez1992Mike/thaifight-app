92237fb11bba7a00b2371d88c5c75e3078a2720c
MIchael Fernandez <michaelfernandez@MIchaels-MBP.home>
2020-07-21 22:10:03 -0400

    commit test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Clone Test</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
